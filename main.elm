e679c26e2017da0b8d9f06e52478d62e50e72320
Ryan Haskell-Glatz <rhaskellglatz@onenorth.com>
2016-10-12 23:21:30 -0700

    create simple html interface to update model

--- main.elm
+++ main.elm
@@ -1,10 +1,50 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.App as App
+import Html.Events exposing (onClick)
 
 import Direction exposing (..)
 import Ship exposing (..)
 
-newShip = (Ship.move SOUTHWEST initShip)
 
-main = text (toString newShip)
+main = App.beginnerProgram
+  { update = update
+  , view = view
+  , model = model
+  }
+
+
+-- MODEL
+type alias Model = { ship: Ship }
+
+model: Model
+model = Model initShip
+
+
+-- UPDATE
+type Msg = Move Direction | None
+
+update: Msg -> Model -> Model
+update msg model =
+  case msg of
+    Move dir ->
+      { model | ship = Ship.move dir model.ship }
+    None ->
+      model
+
+
+-- VIEW
+view: Model -> Html Msg
+view model =
+  div [] 
+  [ button [onClick (Move NORTH)] [text "NORTH"]
+  , button [onClick (Move NORTHEAST)] [text "NORTHEAST"]
+  , button [onClick (Move EAST)] [text "EAST"]
+  , button [onClick (Move SOUTHEAST)] [text "SOUTHEAST"]
+  , button [onClick (Move SOUTH)] [text "SOUTH"]
+  , button [onClick (Move SOUTHWEST)] [text "SOUTHWEST"]
+  , button [onClick (Move WEST)] [text "WEST"]
+  , button [onClick (Move NORTHWEST)] [text "NORTHWEST"]
+  , text (toString model.ship)
+  ]
