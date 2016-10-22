59f30d3f405bc2fe520dceb8e6affc9176eb9e93
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-10-21 23:42:19 -0500

    add pirate collision and disable input

--- Direction.elm
+++ Direction.elm
@@ -1,83 +1,80 @@
-module Direction exposing
-  ( Direction(..)
-  , getIndex
-  , getTransformRotation
-  , getSideDirections
-  , rotateClockwise
-  )
+module Direction
+    exposing
+        ( Direction(..)
+        , getAngle
+        , getSideDirections
+        , rotateClockwise
+        )
 
-import Debug
 import Maybe
-
 import List.Extra
 
 
 type Direction
-  = NORTH
-  | NORTHEAST
-  | EAST
-  | SOUTHEAST
-  | SOUTH
-  | SOUTHWEST
-  | WEST
-  | NORTHWEST
+    = NORTH
+    | NORTHEAST
+    | EAST
+    | SOUTHEAST
+    | SOUTH
+    | SOUTHWEST
+    | WEST
+    | NORTHWEST
 
+
+directionList : List Direction
 directionList =
-  [ NORTH
-  , NORTHEAST
-  , EAST
-  , SOUTHEAST
-  , SOUTH
-  , SOUTHWEST
-  , WEST
-  , NORTHWEST
-  ]
+    [ NORTH
+    , NORTHEAST
+    , EAST
+    , SOUTHEAST
+    , SOUTH
+    , SOUTHWEST
+    , WEST
+    , NORTHWEST
+    ]
 
-getIndex: Direction -> Maybe Int
+
+getIndex : Direction -> Maybe Int
 getIndex dir =
-  List.Extra.elemIndex dir directionList
+    List.Extra.elemIndex dir directionList
 
-getTransformRotation: Direction -> String
-getTransformRotation dir =
-  let
-    prefix =
-      "rotate("
-    suffix =
-      "deg)"
-    angle =
-      case (getIndex dir) of
+
+getAngle : Direction -> Int
+getAngle dir =
+    case (getIndex dir) of
         Just index ->
-          index*45
-        Nothing ->
-          --Debug.log "Direction missing from list: " dir
-          0
-  in
-    prefix ++ (toString angle) ++ suffix
+            index * 45
 
-getSideDirections: Direction -> (Direction, Direction)
+        Nothing ->
+            0
+
+
+getSideDirections : Direction -> ( Direction, Direction )
 getSideDirections dir =
-  let
-    index =
-      case getIndex dir of
-        Just num ->
-          num
-        Nothing ->
-          0
-    numDirections =
-      List.length directionList
-    (leftSideIndex, rightSideIndex) =
-      ( (index + 2) % numDirections, (index + 6) % numDirections )
-  in
-    ( Maybe.withDefault NORTH (List.Extra.getAt leftSideIndex directionList)
-    , Maybe.withDefault NORTH (List.Extra.getAt rightSideIndex directionList)
-    )
+    let
+        index =
+            Maybe.withDefault 0 (getIndex dir)
 
-rotateClockwise: Direction -> Direction
+        numDirections =
+            List.length directionList
+
+        ( leftSideIndex, rightSideIndex ) =
+            ( (index + 2) % numDirections
+            , (index + 6) % numDirections
+            )
+    in
+        ( Maybe.withDefault NORTH (List.Extra.getAt leftSideIndex directionList)
+        , Maybe.withDefault NORTH (List.Extra.getAt rightSideIndex directionList)
+        )
+
+
+rotateClockwise : Direction -> Direction
 rotateClockwise dir =
-  let
-    index =
-      Maybe.withDefault 0 (getIndex dir)
-    newDirection =
-      Maybe.withDefault NORTH (List.Extra.getAt ( (index + 1) % (List.length directionList) ) directionList)
-  in
-    newDirection
+    let
+        index =
+            Maybe.withDefault 0 (getIndex dir)
+
+        newDirection =
+            Maybe.withDefault NORTH (List.Extra.getAt ((index + 1) % (List.length directionList)) directionList)
+    in
+        newDirection
--- RandomHelper.elm
+++ RandomHelper.elm
@@ -1,19 +1,20 @@
-module RandomHelper exposing
-  ( makeSeeds )
+module RandomHelper exposing (makeSeeds)
 
 import Random
 
-makeSeeds: Random.Seed -> Int -> List Random.Seed
-makeSeeds seed numSeeds =
-  makeSeedsHelper seed numSeeds 0 []
 
-makeSeedsHelper: Random.Seed -> Int -> Int -> List Random.Seed -> List Random.Seed
+makeSeeds : Random.Seed -> Int -> List Random.Seed
+makeSeeds seed numSeeds =
+    makeSeedsHelper seed numSeeds 0 []
+
+
+makeSeedsHelper : Random.Seed -> Int -> Int -> List Random.Seed -> List Random.Seed
 makeSeedsHelper seed maxIndex index seeds =
-  let
-    (num, newSeed) =
-      Random.step (Random.int Random.minInt Random.maxInt) seed
-  in
-    if index == maxIndex then
-      seeds
-    else
-      makeSeedsHelper newSeed maxIndex (index+1) (List.append seeds [newSeed])
+    let
+        ( num, newSeed ) =
+            Random.step (Random.int Random.minInt Random.maxInt) seed
+    in
+        if index == maxIndex then
+            seeds
+        else
+            makeSeedsHelper newSeed maxIndex (index + 1) (List.append seeds [ newSeed ])
